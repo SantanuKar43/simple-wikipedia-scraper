e5b7137792ed1fe350b7588dad5049a794bf1c9b
SantanuKar43 <kar.santanu43@gmail.com>
2017-06-06 01:30:54 +0530

    added promises to better handle different responses

--- index.js
+++ index.js
@@ -10,9 +10,32 @@ var request = require('request');
 //     console.log(string.match(/<title>(.*)<\/title>/g));
 //   });
 // }).end();
-
-request.get('https://en.wikipedia.org/wiki/bmw',function(err,response,body){
-    if(err && err.code=="ETIMEDOUT") return console.log("Request timed out");
-    if(!err && response.statusCode==404) return console.log("error 404 occured")
-  console.log(body.match(/<p><b>(.*)<\/p>/)[0].replace(/<[^<]+>/g,""));
+var input = process.argv.slice(2).join("_");
+console.log(input);
+var reqpro = new Promise(function(resolve,reject){
+  request.get('https://en.wikipedia.org/wiki/'+input,function(err,response,body){
+    if(err && err.code=="ETIMEDOUT") return console.log("request timed out");
+    if(response.statusCode==404) return reject(body);
+    else if(response.statusCode==200) return resolve(body);
+    else return console.log("Something went wrong! Try again, please.");
+  });
 });
+
+reqpro.then(function(body){
+  var match = body.match(/<p><b>(.*)<\/p>/);
+  var noLinks = match[1].replace(/<[^<]+>/g,"");
+  var final = noLinks.replace(/\([^\:]*\:[^\:]*\)/g,"");
+  console.log(final);
+},function(body){
+  var searchUrl = body.match(/<a.*href="([^<]*)".*>Search for/);
+  return console.log("please visit-"+searchUrl[1]);
+});
+
+// request.get('https://en.wikipedia.org/wiki/amitabhhbachhan',function(err,response,body){
+//     if(err && err.code=="ETIMEDOUT") return console.log("Request timed out");
+//     if(!err && response.statusCode==404) {
+//       var searchUrl = body.match(/<a.*href="([^<]*)".*>Search for/);
+//       return console.log("please visit-"+searchUrl[1]);
+//     }
+//   console.log(body.match(/<p><b>(.*)<\/p>/)[0].replace(/<[^<]+>/g,""));
+// });
